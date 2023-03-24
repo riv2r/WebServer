75df81aefa8a82e57eb00c5aa9fd146ea49dbce2
riv2r <3237208018@qq.com>
2023-03-24 17:14:11 +0800

    update first version about C/S

--- WebServer.cpp
+++ WebServer.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+// memset
+#include <cstring>
 #include <csignal>
 #include <sys/socket.h>
+#include <sys/types.h>
+// sockaddr_in
 #include <arpa/inet.h>
+// close(fd)
+#include <unistd.h>
 
 using namespace std;
 
@@ -25,8 +31,8 @@ int main(int argc, char const *argv[])
     }
 
     // 建立服务端套接字
-    int serverfd=socket(PF_INET,SOCK_STREAM,0);
-    if(serverfd==-1)
+    int sockfd=socket(PF_INET,SOCK_STREAM,0);
+    if(sockfd==-1)
     {
         cout<<"Error: socket"<<endl;
         return -1;
@@ -36,25 +42,60 @@ int main(int argc, char const *argv[])
     const char* ip=argv[1];
     int port=atoi(argv[2]);
 
-    struct sockaddr_in serverAddr;
-    serverAddr.sin_family=AF_INET;
-    inet_pton(AF_INET,ip,&serverAddr.sin_addr);    
-    serverAddr.sin_port=htons(port);
+    struct sockaddr_in sockAddr;
+    sockAddr.sin_family=AF_INET;
+    inet_pton(AF_INET,ip,&sockAddr.sin_addr);    
+    sockAddr.sin_port=htons(port);
 
-    if(bind(serverfd,(struct sockaddr*)&serverAddr,sizeof(serverAddr))==-1)
+    if(bind(sockfd,(struct sockaddr*)&sockAddr,sizeof(sockAddr))==-1)
     {
         cout<<"Error: bind"<<endl;
         return -1;
     }
 
     // 监听
-    if(listen(serverfd,5)==-1)
+    if(listen(sockfd,5)==-1)
     {
         cout<<"Error: listen"<<endl;
         return -1;
     }
 
-    // 接受连接
+    cout<<"...Listening"<<endl;
 
+    // 接受连接
+    int connfd;
+    char clientIP[INET_ADDRSTRLEN];
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrLen=sizeof(clientAddr);
+
+    while(true)
+    {
+        connfd=accept(sockfd,(struct sockaddr*)&clientAddr,&clientAddrLen);
+        if(connfd<0) cout<<"Error: accept"<<endl;
+        inet_ntop(AF_INET,&clientAddr.sin_addr,clientIP,INET_ADDRSTRLEN);
+        cout<<"...connect "<<clientIP<<":"<<clientAddr.sin_port<<endl;
+
+        char buf[BUF_SIZE];
+        
+        while(true)
+        {
+            memset(buf,0,sizeof(buf));
+            int ret=recv(connfd,buf,sizeof(buf),0);
+            if(ret<=0) 
+            {
+                cout<<"...disconnect "<<clientIP<<":"<<clientAddr.sin_port<<endl;
+                break;
+            }
+            buf[ret]='\0';
+            if(strcmp(buf,"exit")==0)
+            {
+                cout<<"...disconnect "<<clientIP<<":"<<clientAddr.sin_port<<endl;
+                break;
+            }
+            cout<<buf<<endl;
+        }
+        close(connfd);
+    }
+    close(sockfd);
     return 0;
 }
