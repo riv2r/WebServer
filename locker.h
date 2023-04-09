cdd595338b4ae08159c2749c590cd60d4015e5ce
riv2r <3237208018@qq.com>
2023-04-09 20:00:06 +0800

    add locker and threadpool

--- locker.h
+++ locker.h
@@ -3,6 +3,7 @@
 
 #include <exception>
 #include <semaphore.h>
+#include <pthread.h>
 
 // 信号量
 class sem
@@ -28,6 +29,63 @@ class sem
     }
     private:
     sem_t m_sem;
+};
+
+class locker
+{
+    public:
+    locker()
+    {
+        if(pthread_mutex_init(&m_mutex,NULL)!=0) throw std::exception();
+    }
+    ~locker()
+    {
+        pthread_mutex_destroy(&m_mutex);
+    }
+    bool lock()
+    {
+        return pthread_mutex_lock(&m_mutex)==0;
+    }
+    bool unlock()
+    {
+        return pthread_mutex_unlock(&m_mutex)==0;
+    }
+    private:
+    pthread_mutex_t m_mutex;
+};
+
+class cond
+{
+    public:
+    cond()
+    {
+        if(pthread_mutex_init(&m_mutex,NULL)!=0) throw std::exception();
+        if(pthread_cond_init(&m_cond,NULL)!=0)
+        {
+            pthread_mutex_destroy(&m_mutex);
+            throw std::exception();
+        }
+    }
+    ~cond()
+    {
+        pthread_mutex_destroy(&m_mutex);
+        pthread_cond_destroy(&m_cond);
+    }
+    bool wait()
+    {
+        int ret=0;
+        pthread_mutex_lock(&m_mutex);
+        ret=pthread_cond_wait(&m_cond,&m_mutex);
+        pthread_mutex_unlock(&m_mutex);
+        return ret==0;   
+    }
+    bool signal()
+    {
+        return pthread_cond_signal(&m_cond)==0;
+    }
+    private:
+    pthread_mutex_t m_mutex;
+    pthread_cond_t m_cond;
 };
 
 #endif
